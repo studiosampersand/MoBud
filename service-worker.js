d493e3def3001b1aea250268e7ccc92ef2ef31b8
Sam Van Royen <studiosampersand@users.noreply.github.com>
2026-07-09 01:02:05 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-const CACHE='mobud-production-v0-000-007';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./config.js','./i18n.js','./manifest.json','./content/faq.json','./content/mobility-providers.json','./icon-192-v005.png','./icon-512-v005.png','./icon-maskable-512-v005.png'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting()});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
